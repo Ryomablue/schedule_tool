2cb8d2c9ebc943279c7486f699439adb805b89fb
Andrew H <flclfan12@gmail.com>
2020-02-19 21:29:08 -0500

    Basic iteration and if/else to tell if meetings will fit in 8 hr day

--- schedule_tool.rb
+++ schedule_tool.rb
@@ -29,3 +29,31 @@
 # OUTPUT example 2:
 # No, can’t fit.
 
+$hoursInDay = 8
+$meetingCollection =  {name: "Meeting 1", duration: 3, type: 'onsite'}, 
+          {name: "Meeting 2", duration: 2, type: 'offsite'}, 
+          {name: "Meeting 2", duration: 1, type: 'offsite'},
+          {name: "Meeting 2", duration: 0.5, type: 'onsite'}
+
+class Tool
+  #iterate through the meetingCollection of meetingCollection
+   $meetingCollection.each do |key,value|
+    if key[:type] == 'offsite'
+      duration = key[:duration] + 0.5
+    else
+      duration = key[:duration]
+    end
+    #subtract hoursInDay from total in day
+    remainingHoursInDay = $hoursInDay - duration
+    $hoursInDay = remainingHoursInDay
+  end
+
+
+  # simple if/else for if meetings can fit in day
+  if $hoursInDay >= 0
+    puts 'Yes, can fit'
+  else
+    puts 'No, can’t fit'
+  end
+
+end
