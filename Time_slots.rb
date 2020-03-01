10f87d2b62591955db048820a80aea00cacfc03a
Andrew H <flclfan12@gmail.com>
2020-02-29 23:14:42 -0500

    Refactor and add mew timeslot method
    Removed unneeded code, refactored some existing code, and started on a new method to set the timeslots.

--- Time_slots.rb
+++
@@ -1,7 +0,0 @@
-class Time_slots
-	attr_accessor :start_time, :end_time
-	def initialize(start_time, end_time)
-		@start_time = start_time
-		@end_time = end_time
-	end
-end
--- planner.rb
+++ planner.rb
@@ -1,67 +1,78 @@
-# Create new feature that arranges meetingCollection in 8 hour day
-# If meetingCollection cannot fit, let user know "No, can't fit"
-# 2 kinds of meetingCollection: onsite/offsite
-# onsite: can be back to back w/o gaps
-# offsite: 	-30 min buffer on either end (travel time)
-# 			    -can overlap off-site meetingCollection
-# 			    -can extends past Start/End of Day
+  require 'time'
+  require_relative 'Meeting'
+  require_relative 'Scheduler'
 
+  $meetings = {
+    :m1 => Meeting.new("Meeting 1", 1.5, :onsite),
+    :m2 => Meeting.new('Meeting 2', 2, :offsite),
+    :m3 => Meeting.new('Meeting 3', 1, :onsite),
+    :m4 => Meeting.new('Meeting 4', 1, :offsite),
+    :m5 => Meeting.new('Meeting 5', 1, :offsite)
+  }
 
-require 'time'
-require_relative 'Meeting'
-require_relative 'Scheduler'
-require_relative 'Time_slots'
+class Planner
+  t = Scheduler.new
+  $hoursInDay = t.time_in_day
 
-t = Scheduler.new
-$hoursInDay = t.time_difference
-
-$meetings = {
-  :m1 => Meeting.new("Meeting 1", 1.5, :onsite),
-  :m2 => Meeting.new('Meeting 2', 2, :offsite),
-  :m3 => Meeting.new('Meeting 3', 1, :onsite),
-  :m4 => Meeting.new('Meeting 4', 1, :offsite),
-  :m5 => Meeting.new('Meeting 5', 1, :offsite)
-}
-
-$timeslots = {
-  :t1 => Time_slots.new("9:00", "10:30"),
-  :t2 => Time_slots.new("10:30", "11:30"),
-  :t3 => Time_slots.new("12:00", "1:00"),
-  :t4 => Time_slots.new("1:30", "2:30"),
-  :t5 => Time_slots.new("3:00", "5:00")
-}
-
-def can_fit_all_meetings?
-  $meetings.each do |key,value|
-    case value.type
-    when :offsite
-      duration = value.duration + 0.5
+  def schedule_my_meetings
+    case can_fit_all_meetings?
+    when 0..8
+      puts sort_meetings
     else
-      duration = value.duration
+      puts 'No, can’t fit'
     end
-    remainingHoursInDay = $hoursInDay - duration
-    $hoursInDay = remainingHoursInDay
   end
 
-  return $hoursInDay
-end
+  def can_fit_all_meetings?
+    $meetings.each do |key,value|
+      case value.type
+      when :offsite
+        duration = value.duration + 0.5
+      else
+        duration = value.duration
+      end
+      remainingHoursInDay = $hoursInDay - duration
+      $hoursInDay = remainingHoursInDay
+    end
 
-def set_timeslots
+    return $hoursInDay
+  end
 
-end
+  def set_timeslots(sorted_meetings)
+    t1 = Scheduler.new
+    arr = []
+    prev_end_time = Time.parse("00:00")
+    first_meeting = 0 
 
-def sort_meetings
-  $meetings.each do |key, value|
-    puts "timeslots - #{value.name}"
+    sorted_meetings.each do |key,value|
+      result = t1.schedule(value[:duration], first_meeting, prev_end_time, value[:type])
+      arr << "#{result[:start_time].strftime('%H:%M')} - #{result[:end_time].strftime('%H:%M')} - #{key}"
+      first_meeting = first_meeting + 1
+      prev_end_time = result[:end_time]
+    end
+    return arr
+  end
+
+  def sort_meetings
+    onsite = Hash.new
+    offsite = Hash.new
+
+    $meetings.each do |key,value|
+      meeting = value.name
+      duration = value.duration
+      type = value.type
+      meet_details = {:duration => duration, :type => type}
+
+      if type == :onsite
+        onsite.store(meeting, meet_details)
+      else
+        offsite.store(meeting, meet_details)
+      end
+    end
+    sorted_meetings = onsite.merge(offsite)
+    scheduled_meetings = set_timeslots(sorted_meetings)
   end
 end
 
-
-# Let user know that meetingCollection fit in day
-  case can_fit_all_meetings?
-  when 0..8
-    set_timeslots
-    sort_meetings
-  else
-    puts 'No, can’t fit'
-  end
+test1 = Planner.new
+test1.schedule_my_meetings
